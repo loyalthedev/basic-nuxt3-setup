3f9dccaa96ce7cea34ac4c6bda0c60cac98054ad
loyalthedev <lifeofemmany6@gmail.com>
2025-09-01 22:37:54 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="IndexPage" class="w-full">
+    <h1 class="text-4xl">Index Page!!</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+</script>
+
+<style scoped></style>
